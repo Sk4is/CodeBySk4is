2a98013f61b06c13af1b79639e8c4ffc43fc56c1
Adrián Pérez Agredano <adrianperezagredano@gmail.com>
2025-08-11 01:59:28 +0200

    background

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Background.css";
+
+export default function Background() {
+  return (
+    <div className="bg-fixed">
+      <img src="/images/fondoMono (4).png" alt="Fondo" className="bg-image" />
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Background from '../components/Background/Background';
+
+const Home = () => {
+
+  return (
+    <div>
+      <Background />
+    </div>
+  );
+};
+
+export default Home;
